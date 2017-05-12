0ae891235754827d903f3da3797618edfd5fb928
Julien Baeriswyl <julien.baeriswyl@heig-vd.ch>
2017-05-12 18:44:28 +0000

    STEP3: add basic proxy

--- manager.sh
+++ manager.sh
@@ -8,6 +8,11 @@ f_image_stop ()
     docker rm $(docker ps -a -q --filter ancestor=$1)
 }
 
+f_docker_net ()
+{
+    docker network create --subnet=172.18.0.0/16 resnet13
+}
+
 f_build ()
 {
     local repo=$(dirname $(readlink -f $0))
@@ -17,22 +22,29 @@ f_build ()
     
     cd $repo/docker-images/express-image
     docker build -t res/express_students .
+    
+    cd $repo/docker-images/reverse-proxy-image
+    docker build -t res/apache_rp .
 }
 
 f_start ()
 {
-    docker run -d -p 9090:80   res/apache_php
-    docker run -d -p 9091:3000 res/express_students
+    docker run -d --net resnet13 --ip 172.18.0.2 res/apache_php
+    docker run -d --net resnet13 --ip 172.18.0.3 res/express_students
+    docker run -d --net resnet13 -p 9090:80 res/apache_rp
 }
 
 f_stop ()
 {
-    foreach f_image_stop res/apache_php res/express_students
+    foreach f_image_stop res/apache_php res/express_students res/apache_rp
 }
 
 echo '#### RES-B : Lab - HTTPIntra ####'
 
 case $1 in
+    --net)
+        f_docker_net
+        ;;
     --start)
         f_build
         f_start
